0dad7ad5ccf3a0fc61e3ab7af46a5ab64126fc45
Worawit-Nantasu <worawit.n@ku.th>
2024-04-12 14:44:16 +0700

    Update UserInfo.jsx

--- SE_PROJ_2/src/pages/UserInfo/UserInfo.jsx
+++ SE_PROJ_2/src/pages/UserInfo/UserInfo.jsx
@@ -4,8 +4,23 @@ import './UserInfo.css';
 
 const UserInfo = () => {
     return (
-        <div className="center">
-            <img src={sosImage} alt="SOS" />
+        <div className="userInfo-container">
+            <div className="circle"></div>
+
+            <div className="box box-name"></div>
+            <div className="text text-name">ชื่อ</div>
+
+            <div className="box box-surname"></div>
+            <div className="text text-surname">นามสกุล</div>
+
+            <div className="box box-faculty"></div>
+            <div className="text text-faculty">คณะและสาขา</div>
+
+            <div className="box box-phone"></div>
+            <div className="text text-phone">เบอร์โทรศัพท์</div>
+
+            <div className="box box-email"></div>
+            <div className="text text-email">อีเมล</div>
         </div>
     );
 };
