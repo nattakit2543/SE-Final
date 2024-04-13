f29240033c5aa056dd875139cd99ab0fa2d15be7
Worawit-Nantasu <worawit.n@ku.th>
2024-04-13 07:52:24 +0700

    Update HowToUseT.jsx

--- SE_PROJ_2/src/pages/2Teacher/HowToUseT.jsx
+++ SE_PROJ_2/src/pages/2Teacher/HowToUseT.jsx
@@ -1,13 +1,39 @@
 import React from 'react';
-import sosImage from '../../assets/SOS.png';
-import './Style.css';
+import howToUsePDF from '../../assets/howTOuse.pdf';
 
-const HowToUseT = () => {
+function HowToUseT() {
+    const containerStyle = {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: 'calc(100vh - 20px)', 
+        width: 'calc(100vw - 65px)',
+        marginTop: '50px', 
+
+    };
+
+    const pdfStyle = {
+        width: '100%', 
+        height: '100%', 
+        maxWidth: '2000px', 
+        maxHeight: '1000px', 
+        border: 'none' 
+    };
+
     return (
-        <div className="center">
-            <img src={sosImage} alt="SOS" />
+        <div style={containerStyle}>
+            <object 
+                data={howToUsePDF} 
+                type="application/pdf" 
+                style={pdfStyle}
+                aria-label="Document"
+            >
+                <p>Your browser does not support PDFs. 
+                    <a href={howToUsePDF}>Download the PDF</a>.
+                </p>
+            </object>
         </div>
     );
-};
+}
 
 export default HowToUseT;
