c0e4e7ccbdf573fd60f59c53b6c16cf86f63cfb7
Worawit-Nantasu <worawit.n@ku.th>
2024-04-15 03:56:19 +0700

    สร้างดรอปดาวน์เมนู

--- SE_PROJ_2/src/pages/2Teacher/componentsT/PopUpReqSub.jsx
+++ SE_PROJ_2/src/pages/2Teacher/componentsT/PopUpReqSub.jsx
@@ -46,7 +46,7 @@ function PopUpReqSub({ isOpen, formData, onClose, onSubmit }) {
       courseCategory: value => !isEmptyString,
       classYear: value => !isEmptyString
     };
-    return rules[name] ? rules[name](value) : true; // Return true if no rule exists
+    return rules[name] ? rules[name](value) : true;
   };
 
   const handleChange = (e) => {
@@ -65,9 +65,9 @@ function PopUpReqSub({ isOpen, formData, onClose, onSubmit }) {
   const handleSubmission = () => {
     const allFieldsValid = Object.entries(localFormData).every(([key, value]) => validateField(key, value));
     if (allFieldsValid) {
-      onSubmit(localFormData); // Only submit if all validations pass
+      onSubmit(localFormData);
     } else {
-      alert('Some fields are invalid. Please correct them and try again.'); // Show error message or handle validation errors appropriately
+      alert('Some fields are invalid. Please correct them and try again.');
     }
   };
 
@@ -85,14 +85,24 @@ function PopUpReqSub({ isOpen, formData, onClose, onSubmit }) {
       <input type="text" name="courseNameEN" placeholder="ชื่อวิชา(EN)" value={localFormData.courseNameEN} onChange={handleChange} className={`input-field large ${fieldErrors.courseNameEN ? 'error' : ''}`} />
       <input type="text" name="courseNameTH" placeholder="ชื่อวิชา(TH)" value={localFormData.courseNameTH} onChange={handleChange} className={`input-field large ${fieldErrors.courseNameTH ? 'error' : ''}`} />
       <input type="text" name="prevCourse" placeholder="วิชาพื้นฐาน" value={localFormData.prevCourse} onChange={handleChange} className={`input-field large ${fieldErrors.prevCourse ? 'error' : ''}`} />
-      <input type="text" name="courseCategory" placeholder="หมวดวิชา" value={localFormData.courseCategory} onChange={handleChange} className={`input-field large ${fieldErrors.courseCategory ? 'error' : ''}`} />
+      <select name="courseCategory" value={localFormData.courseCategory} onChange={handleChange} className={`input-field larges ${fieldErrors.courseCategory ? 'error' : ''}`}>
+        <option value="">เลือกหมวดวิชา</option>
+        <option value="วิชาศึกษาทั่วไป">วิชาศึกษาทั่วไป</option>
+        <option value="วิชาเฉพาะบังคับ">วิชาเฉพาะบังคับ</option>
+        <option value="วิชาเฉพาะเลือก">วิชาเฉพาะเลือก</option>
+      </select>
       <div className="input-group">
         <input type="text" name="credits" placeholder="หน่วยกิต" value={localFormData.credits} onChange={handleChange} className={`input-field small ${fieldErrors.credits ? 'error' : ''}`} />
         <input type="text" name="numberOfStudents" placeholder="จำนวนนิสิต" value={localFormData.numberOfStudents} onChange={handleChange} className={`input-field small ${fieldErrors.numberOfStudents ? 'error' : ''}`} />
       </div>
       <div className="input-group">
         <input type="text" name="numberOfGroups" placeholder="จำนวนหมู่เรียน" value={localFormData.numberOfGroups} onChange={handleChange} className={`input-field small ${fieldErrors.numberOfGroups ? 'error' : ''}`} />
-        <input type="text" name="classYear" placeholder="ชั้นปีที่เปิดสอน" value={localFormData.classYear} onChange={handleChange} className={`input-field small ${fieldErrors.classYear ? 'error' : ''}`} />
+        <select name="classYear" value={localFormData.classYear} onChange={handleChange} className={`class-year-select ${fieldErrors.classYear ? 'error' : ''}`}>
+          <option value="">ชั้นปีที่เปิดสอน</option>
+          <option value="T12(1)">T12(1)</option>
+          <option value="T12(2)">T12(2)</option>
+          <option value="T12(2)">T12(3)</option>
+        </select>
       </div>
       <button className="button" onClick={handleSubmission}>ส่งคำร้อง</button>
     </div>
