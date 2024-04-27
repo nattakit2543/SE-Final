5c1511e9bc5f5df144e132de64f5a9d3d6fbb631
Worawit-Nantasu <worawit.n@ku.th>
2024-04-27 19:14:12 +0700

    Update PopUpReqSub.jsx
    ขอลบก่อนเชื่อมหลัง

--- SE_PROJ_2/src/pages/2Teacher/componentsT/PopUpReqSub.jsx
+++ SE_PROJ_2/src/pages/2Teacher/componentsT/PopUpReqSub.jsx
@@ -63,28 +63,10 @@ function PopUpReqSub({ isOpen, formData, onClose, onSubmit }) {
     }));
   };
 
-  const handleSubmission = async () => {
+  const handleSubmission = () => {
     const allFieldsValid = Object.entries(localFormData).every(([key, value]) => validateField(key, value));
     if (allFieldsValid) {
-      try {
-        const response = await fetch('http://localhost:3100/api/requests', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify(localFormData)
-        });
-        if (response.ok) {
-          const result = await response.json();
-          console.log(result.message);
-          onSubmit();
-          onClose(); 
-        } else {
-          console.error('Failed to submit request');
-        }
-      } catch (error) {
-        console.error('Error submitting the request:', error);
-      }
+      onSubmit(localFormData);
     } else {
       const updatedErrors = Object.keys(localFormData).reduce((errors, field) => {
         errors[field] = !validateField(field, localFormData[field]);
