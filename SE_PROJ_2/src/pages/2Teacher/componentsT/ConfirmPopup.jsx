920f11d0634eb138497bed24b301a35ad1150c09
Worawit-Nantasu <worawit.n@ku.th>
2024-04-28 16:27:20 +0700

    Update ConfirmPopup.jsx

--- SE_PROJ_2/src/pages/2Teacher/componentsT/ConfirmPopup.jsx
+++ SE_PROJ_2/src/pages/2Teacher/componentsT/ConfirmPopup.jsx
@@ -5,13 +5,12 @@ import { IoMdWarning } from 'react-icons/io';
 const Overlay = styled.div`
   position: fixed;
   top: 0;
-  left: 65px;
+  left: 0;
   right: 0;
   bottom: 0;
   display: flex;
   align-items: center;  
   justify-content: center;  
-  background-color: rgba(0, 0, 0, 0.5);
   z-index: 1000;
 `;
 
